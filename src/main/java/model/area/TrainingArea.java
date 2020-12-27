59e86a21b2344bfe06b6fd85a6931e49f77f3d3a
Andrey Egorov <51491580+myfosse@users.noreply.github.com>
2020-12-27 13:22:09 +0300

    Update TrainingArea.java

--- src/main/java/model/area/TrainingArea.java
+++ src/main/java/model/area/TrainingArea.java
@@ -11,6 +11,7 @@ import java.util.Set;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class TrainingArea<T extends Animal> {
     private int square;
     private int amountOfObstacles;
